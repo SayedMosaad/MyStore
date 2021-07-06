f3fee3eae506ecd18cc6f8844722f5c2ee05faf1
sayed mosaad <sayed.mosaad920@gmail.com>
2021-07-06 13:15:27 +0200

    adding Domain.Core and Implementation of InMemoryBus

---
+++ MyStore/MyStore.Infra.Bus/InMemoryBus.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using MyStore.Domain.Core.Bus;
+using MyStore.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Infra.Bus
+{
+    public class InMemoryBus : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T:Command
+        {
+            return _mediator.Send(command);
+        }
+    }
+}
